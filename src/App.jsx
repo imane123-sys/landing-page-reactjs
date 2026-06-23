71166d1314542c61ccbdb5923d8027afa767148a
Imane Ramadane <ramadaneimane@gmail.com>
2026-06-23 18:00:22 +0100

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import Header from "./components/Header";
-import './App.css'
+import "./App.css";
 function App() {
   return (
     <>
       <Header />
+      <div className="bg"></div>
     </>
   );
 }
